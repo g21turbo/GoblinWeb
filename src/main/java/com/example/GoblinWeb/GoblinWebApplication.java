5b5a4e4e18fdb5f0a41402bf4093490e1c82918b
User <dg21stew@gmail.com>
2023-03-16 20:50:51 -0700

    Initial commit

---
+++ src/main/java/com/example/GoblinWeb/GoblinWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.GoblinWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoblinWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoblinWebApplication.class, args);
+	}
+
+}
